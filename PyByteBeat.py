b09374a077ad01fb1b9d70f454066e5cd2c1b245
The Milker Guy <notthemilkerguy@gmail.com>
2025-01-03 19:19:44 +0700

    Fixes some bugs :D

--- PyByteBeat.py
+++ PyByteBeat.py
@@ -1,7 +1,35 @@
+from math import *
+from random import random as rand
+
+def div(a, b): return a // b if b else 0
+def truediv(a, b): return a / b if b else 0
+def b_and(a, b): return int(a) & int(b)
+def b_or(a, b): return int(a) | int(b)
+def rs(a, b): return int(a) >> int(b)
+def ls(a, b): return int(a) << int(b)
+def pw(a, b): return a ** b
+
 def _buffer(eq, s, k=8000):
     buffer = bytearray()
     for t in range(int(k) * s):
-        sample = abs(int(eval(eq)))
+        try:
+            sample = abs(int(eval(eq, {'acos': acos,'acosh': acosh,'asin': asin,'asinh': asinh,'atan': atan,'atan2': atan2,
+        'atanh': atanh,'cbrt': cbrt,'ceil': ceil,'comb': comb,'copysign': copysign,'cos': cos,
+        'cosh': cosh,'degrees': degrees,'dist': dist,'e': 2.718281828459045,'erf': erf,'erfc': erfc,'exp': exp,
+        'exp2': exp2,'expm1': expm1,'fabs': fabs,'factorial': factorial,'floor': floor,'fma': fma,
+        'fmod': fmod,'frexp': frexp,'fsum': fsum,'gamma': gamma,'gcd': gcd,'hypot': hypot,'inf': inf,
+        'isclose': isclose,'isfinite': isfinite,'isinf': isinf,'isnan': isnan,'isqrt': isqrt,
+        'lcm': lcm,'ldexp': ldexp,'lgamma': lgamma,'log': log,'log10': log10,'log1p': log1p,'log2': log2,
+        'modf': modf,'nan': nan,'nextafter': nextafter,'perm': perm,'pi': pi,'pow': pow,'prod': prod,
+        'radians': radians,'remainder': remainder,'sin': sin,'sinh': sinh,
+        'sqrt': sqrt,'sumprod': prod,'tan': tan,'tanh': tanh,'tau': tau,'trunc': trunc,'ulp': ulp,
+        'abs': abs, 'int': int, 'round': round, '^': pw, 't': t,
+        '//': div, '/': truediv, '&': b_and, '|': b_or, '>>': rs, '<<': ls
+        })))
+            sample = sample % 256
+        except Exception as e:
+            print(f"Error evaluating equation at t={t}: {e}")
+            sample = 0
         buffer.append(sample & 0xFF)
     return buffer
 
@@ -27,3 +55,9 @@ def _wav(f, buffer, s, k=8000):
     with open(f, 'wb') as f:
         f.write(header)
         f.write(buffer)
+
+equation = ''
+seconds = 10
+buffer = _buffer(equation, seconds)
+output_file = "output.wav"
+_wav(output_file, buffer, seconds)
